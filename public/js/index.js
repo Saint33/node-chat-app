b5d628856f64d4eff1279325f62f1fc11fc48f12
Saint33 <bnsaintz@gmail.com>
2018-03-20 14:25:42 +0300

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from : 'igor',
-        text: 'event listener'
-    })
 });
 
 socket.on('disconnect', function() {
