cf7ab39e0c923877930170622b4b87bb6156f1a5
Saint33 <bnsaintz@gmail.com>
2018-03-21 07:02:14 +0300

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+  
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+      messages.scrollTop(scrollHeight);
+    }
+  }
+
 socket.on('connect', function() {
     console.log('connected to server');
 });
@@ -18,7 +34,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // let li = jQuery('<li></li>');
     // li.text(`${message.from} (${formattedTime}): ${message.text}`);
 
@@ -41,6 +57,7 @@ socket.on('newLocationMessage', function(message){
 
     // li.append(a);
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(event) {
